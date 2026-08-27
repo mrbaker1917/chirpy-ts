0648ee87768c6a1b72e8006aea9def8f4d7d6d80
mrbaker1917 <mrbaker1917@gmail.com>
2026-08-27 09:41:44 -0700

    adds middlewareLogging

---
+++ src/api/middleware.ts
@@ -0,0 +1,11 @@
+import { Request, Response, NextFunction } from "express";
+
+export async function middlewareLogResponses(req: Request, res: Response, next: NextFunction) {
+    res.on("finish", () => {
+        const statusCode = res.statusCode;
+        if (statusCode >= 300) {
+            console.log(`[NON-OK] ${req.method} ${req.url} - Status: ${statusCode}`);
+        }
+    });
+    next();
+}
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import { handlerReadiness } from "./api/readiness.js";
 import express from "express";
+import { middlewareLogResponses } from "./api/middleware.js";
 
 const app = express();
 const port = 8080;
 
+app.use(middlewareLogResponses);
 app.use("/app", express.static("./src/app"));
 
 app.get("/healthz", handlerReadiness);
