9b6cb2c99b65e44c3667081b93e592295849ad1d
mrbaker1917 <mrbaker1917@gmail.com>
2026-08-30 18:01:50 -0700

    adds ability to validate chirp and clean

---
+++ src/api/chirp.ts
@@ -0,0 +1,31 @@
+import type { Request, Response } from "express";
+
+import { respondWithJSON, respondWithError } from "./json.js";
+
+
+export async function handlerChirpsValidate(req: Request, res: Response) {
+    type parameters = {
+        body: string;
+    };
+
+    const params: parameters = req.body;
+    const chirpWords = params.body.split(" ");
+    const curseWords = ["kerfuffle", "sharbert", "fornax"];
+    for (let i = 0; i < chirpWords.length; i++) {
+        for (let cWord of curseWords) {
+            if (chirpWords[i].toLocaleLowerCase() === cWord) {
+                chirpWords[i] = "****";
+            }
+        }
+    }
+    const censoredChirp = chirpWords.join(" ");
+
+    const maxChirpLength = 140;
+    if (params.body.length > maxChirpLength) {
+        respondWithError(res, 400, "Chirp is too long");
+        return;
+    }
+    respondWithJSON(res, 200, {
+        "cleanedBody": censoredChirp,
+    });
+}
--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,20 @@ import express from "express";
 import { handlerReadiness } from "./api/readiness.js";
 import { middlewareLogResponses, middlewareMetricsInc } from "./api/middleware.js";
 import { handlerMetrics } from "./api/metrics.js";
-import { config } from "./config.js";
+import { handlerChirpsValidate } from "./api/chirp.js";
 import { handlerReset } from "./api/reset.js";
 
 const app = express();
 const port = 8080;
 
 app.use(middlewareLogResponses);
+app.use(express.json());
 app.use("/app", middlewareMetricsInc);
 app.use("/app", express.static("./src/app"));
 app.get("/admin/metrics", handlerMetrics);
 app.get("/api/healthz", handlerReadiness);
 app.post("/admin/reset", handlerReset);
+app.post("/api/validate_chirp", handlerChirpsValidate);
 
 async function main() {
     app.listen(port, () => {
