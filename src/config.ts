7cdbf7f302afd8bc6e0f1610cd5083d918ab00e1
mrbaker1917 <mrbaker1917@gmail.com>
2026-09-05 08:27:09 -0700

    links db to app

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,20 @@
 type APIConfig = {
     fileserverHits: number;
+    dbURL: string;
+};
+
+process.loadEnvFile();
+
+
+function envOrThrow(key: string) {
+    const dbURL = process.env[key];
+    if (!dbURL) {
+        throw new Error(`Environment variable ${dbURL} is not set`);
+    }
+    return dbURL;
 };
 
 export const config: APIConfig = {
     fileserverHits: 0,
+    dbURL: envOrThrow("DB_URL")
 };
